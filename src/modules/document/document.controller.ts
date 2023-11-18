74d191dbace204e5841000ef97ab2fe7aa4552d4
yazid138 <ma39isy@gmail.com>
2023-11-18 13:06:34 +0700

    commit

--- src/modules/document/document.controller.ts
+++ src/modules/document/document.controller.ts
@@ -2,14 +2,17 @@ import { Controller, Get, Param, Res } from '@nestjs/common';
 import { Readable } from 'stream';
 import { Response } from 'express';
 import { DocumentService } from './document.service';
+import { ApiTags } from '@nestjs/swagger';
+import { ParseObjectIdPipe } from '../../common/pipes/parse-objectid.pipe';
 
 @Controller('document')
+@ApiTags('Document')
 export class DocumentController {
   constructor(private readonly documentService: DocumentService) {}
 
   @Get(':chunkId/:name')
   async getMedia(
-    @Param('chunkId') chunkId: string,
+    @Param('chunkId', ParseObjectIdPipe) chunkId: string,
     @Param('name') name: string,
     @Res() res: Response,
   ) {
